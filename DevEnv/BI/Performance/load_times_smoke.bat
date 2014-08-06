e6069ba1bead0613cef1e1fc5da8a793f701efcb
jennawakeley <jennawakeley@gmail.com>
2014-08-06 10:22:00 -0500

    1.1.2

--- DevEnv/BI/Performance/load_times_smoke.bat
+++ DevEnv/BI/Performance/load_times_smoke.bat
@@ -2,4 +2,4 @@
 load_times  > load_time_log.txt
 
 
-sendEmail -f esoanalyticsqa@gmail.com -t esoanalyticsqa@gmail@esosolutions.com -bcc jenna.wakeley@esosolutions.com -s smtp.gmail.com:587 -xu esoanalyticsqa@gmail.com -xp .admin77.  -u "Results: (Morning) Daily Load Times of Analytics Reports" -m "Hi buddy, attached are the results of the Morning Daily Load Times of Analytics Reports.  This is an automated email replies to this inbox are not monitored. " -o tls=yes -a "C:\Smoke\BI\Performance\load_time_log.txt" 
+sendEmail -f esoanalyticsqa@gmail.com -t richard.hale@esosolutions.com -bcc jenna.wakeley@esosolutions.com -s smtp.gmail.com:587 -xu esoanalyticsqa@gmail.com -xp .admin77.  -u "Results: Daily Load Times of Analytics Reports" -m "Hi buddy, attached are the results of the Daily Load Times of Analytics Reports.  This is an automated email, replies to this inbox are not monitored. For futher information please contact Jenna.Wakeley@esosolutions.com" -o tls=yes -a "C:\Analytics\AutomatedTesting\DevEnv\BI\Performance\load_time_log.txt" 
